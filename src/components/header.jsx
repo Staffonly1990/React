23916c5bd6b7b6cb3d3e94bd6e1f9331ab6cd09d
Staffonly1990 <56542769+Staffonly1990@users.noreply.github.com>
2021-04-22 16:11:12 +0500

    Study_react_1

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+	return (
+		<header className='Header'>
+        <img src='https://images.ua.prom.st/2514421616_2514421616.jpg?PIMAGE_ID=2514421616'></img>
+      </header>
+	)
+}
+
+export {Header,}
